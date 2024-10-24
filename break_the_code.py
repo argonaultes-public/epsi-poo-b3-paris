8317f138d0755fbafd69ae93f126bb4c7aeb8991
argonaultes <gael@argonaultes.fr>
2024-10-24 19:42:05 +0200

    break the code is working

--- break_the_code.py
+++ break_the_code.py
@@ -11,6 +11,24 @@ def check_pin(user_pin):
             return False
     return True
 
-user_pin = input('Your code: ')
+#user_pin = input('Your code: ')
 
-print(check_pin(user_pin))
+
+def break_the_code():
+    pin_list = 6 * ['0']
+    # pour chaque position
+    for idx in range(len(pin_list)):
+        # tourner la roue en position idx
+        for digit in range(10):
+            pin_list[idx] = str(digit)
+            pin = ''.join(pin_list)
+            start_time = time.time()
+            result = check_pin(pin)
+            end_time = time.time()
+            time_elapsed = int(end_time - start_time)
+            print(f'{time_elapsed} : {pin_list} : {result}')
+            if time_elapsed > 1 + idx or result:
+                break
+    return pin
+
+print(break_the_code())
