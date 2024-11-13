22606cd55947a45d0e6817e921b97345c44cb4a4
argonaultes <gael@argonaultes.fr>
2024-11-13 12:29:33 +0100

    enoncé course

---
+++ race.py
@@ -0,0 +1,8 @@
+
+## Exercice des vaisseaux
+
+# Vous devez modéliser une course de vaisseaux spatiaux.
+# Ces vaisseaux ont comme particularité un nom, une couleur, une vitesse nominale (m/s) et une latence (s). 
+# Les vaisseaux évoluent sur un circuit qui a une distance en mètres et un nombre de tours.
+# Pour une course donnée, à chaque tick (s), indiquer la position des vaisseaux.
+# Sauvegarder la position des vaisseaux en BDD à chaque tick.
