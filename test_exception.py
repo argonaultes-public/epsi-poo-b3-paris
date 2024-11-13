153a0c12634a8e3ee880288b2d7fd410ca1c0a47
argonaultes <gael@argonaultes.fr>
2024-11-13 09:38:45 +0100

    exercice exception todo

---
+++ test_exception.py
@@ -0,0 +1,9 @@
+def ecrire_dans_fichier(file, content):
+    pass
+    # Cette fonction doit ouvrir le fichier en mode ajout et écrire le contenu de content dans le fichier
+
+    # Exception 1 :  fichier introuvable, afficher un message sur la console indiquant que le fichier demandé est introuvable sans provoquer l'arrêt brutal de l'application.
+
+    # Exception 2 : Si le contenu à écrire est invalide (essayer de mettre une valeur numérique), indiquer que le type fourni n'est pas valide sans provoquer l'arrêt brutal de l'application. 
+
+    # Si l'ouverture s'est bien passée, afficher un message indiquant le nombre de caractères qui a été rajouté au fichier.
