5795842efded08f94386cfc4757e9da085d81de7
argonaultes <gael@argonaultes.fr>
2024-11-13 09:59:05 +0100

    correction enonce

--- test_exception.py
+++ test_exception.py
@@ -1,8 +1,20 @@
-def ecrire_dans_fichier(file, content):
-    pass
-    # Cette fonction doit ouvrir le fichier en mode ajout et écrire le contenu de content dans le fichier
+# exemple de contenu de filename_input
+# 12
+# 40
+# 4112
 
-    # Exception 1 :  fichier introuvable, afficher un message sur la console indiquant que le fichier demandé est introuvable sans provoquer l'arrêt brutal de l'application.
+# exemple invalide de contenu de filename_input
+# a12
+# 40
+# 4112
+
+
+def copy_file(filename_input, filename_output):
+    pass
+    # Cette fonction doit ouvrir le fichier filename_output en mode ajout et écrire le contenu du fichier filename_input dans le fichier
+    # A la lecture du fichier filename_input, convertir chaque ligne en entier pour obtenir la somme totale à reporter à la fin du fichier filename_output.
+
+    # Exception 1 :  fichier input introuvable, afficher un message sur la console indiquant que le fichier demandé est introuvable sans provoquer l'arrêt brutal de l'application.
 
     # Exception 2 : Si le contenu à écrire est invalide (essayer de mettre une valeur numérique), indiquer que le type fourni n'est pas valide sans provoquer l'arrêt brutal de l'application. 
 
